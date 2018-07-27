317acb136dda53e210deef56a2cf20d918d5eaaa
lightquencher <lightquencher@gmail.com>
2018-07-27 23:36:24 +1200

    Delete rann.py
    Going to re-upload into folder

--- rann.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-rands1 = np.add(1, np.multiply(2, np.random.rand(1000)))
-rands2 = np.add(1, np.multiply(2, np.random.rand(1000)))
-with open("data_flowers.txt", 'w') as file:
-    for i in range(1000):
-      file.write("{0:.2f} {1:.2f} {2}\n".format(rands1[i], rands2[i], ("1 0" if rands1[i]<rands2[i] else "0 1")))
